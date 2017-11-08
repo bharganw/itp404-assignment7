6c6eb6e802c9366764ac58122989c5c81a852f1a
YashB9 <bharganwar.yash@gmail.com>
2017-11-07 19:30:52 -0800

    vote-control does not work

---
+++ app/controllers/application.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+	actions: {
+		searchForSubreddits(event) {
+			event.preventDefault();
+			let keywords = this.get('keywords'); //Same as the keywords in the .hbs file
+			this.transitionToRoute('subreddit', keywords);
+		}
+	}
+});
